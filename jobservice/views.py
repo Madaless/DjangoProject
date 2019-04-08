8f00ffe289f7c26883c6e439bd47e40a002bf640
Madaless <bartoszmarciszewski555@gmail.com>
2019-04-08 10:47:23 +0200

    Made database

--- jobservice/views.py
+++ jobservice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import JobOffer
 # Create your views here.
 
 joboffers = [
@@ -37,7 +38,7 @@ joboffers = [
 
 def home(request):
     context = {
-        'joboffers' : joboffers
+        'joboffers' : JobOffer.objects.all()
     }
     return render(request,'jobservice/home.html', context)
 
