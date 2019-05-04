7f9e05f415a5d128a6e33c5194964992c6e44397
joannamaria27 <joan.mk7@gmail.com>
2019-05-04 17:38:06 +0200

    update madels

--- jobservice/models.py
+++ jobservice/models.py
@@ -14,6 +14,7 @@ class Company(models.Model):
     companyName = models.CharField(max_length= 50,default="")
     companyMail = models.EmailField(default="")
     companyPassword = models.CharField(max_length= 50,default="") 
+
     def __str__(self):
         return self.companyName
 
@@ -27,15 +28,20 @@ class JobOffer(models.Model):
     location =  models.CharField(max_length=105,default="")
     additionalSkills = models.CharField(max_length=105,default="")
 
+    def __str__(self):
+        return self.title+' '+str(self.companyName)
+
 class Person(models.Model):
     userName = models.CharField(max_length=10,default="")
     personMail = models.EmailField(default="")
     personPassword = models.CharField(max_length=10,default="")
+
     def __str__(self):
         return self.userName
 
 class Cv(models.Model):
-    userName = models.OneToOneField(Person, on_delete = models.CASCADE)
+    nameCv = models.CharField(max_length=50,default="")
+    userName = models.ForeignKey(Person, on_delete = models.CASCADE)
     lastName = models.CharField(max_length=50,default="")
     firstName = models.CharField(max_length=50,default="")
     dateOfBirth = models.CharField(max_length=50,default="")
@@ -44,11 +50,23 @@ class Cv(models.Model):
     experience = models.CharField(max_length=50,default="")
     description = models.CharField(max_length=50,default="")
 
+    def __str__(self):
+        return self.nameCv
+
 class ReplyToOffer(models.Model):
     idPerson = models.ForeignKey(Person, on_delete = models.CASCADE,default="")
     idOffer = models.ForeignKey(JobOffer, on_delete = models.CASCADE,default="")
     dateAdd = models.DateTimeField(auto_now_add=True)
+    cv = models.ForeignKey(Cv, on_delete = models.CASCADE, default="")
+    messForCompany = models.CharField(max_length=150,default="")
+
+    def __str__(self):
+        return str(self.idOffer)+' '+str(self.idPerson)
 
 class FeedbackAnswer(models.Model):
-    accept = models.CharField(max_length=50,default="")
+    idReplyToOffer = models.ForeignKey(ReplyToOffer, on_delete = models.CASCADE,default="")
+    accept = models.BooleanField(max_length=50,default="")
     response = models.CharField(max_length=50,default="")
+
+    def __str__(self):
+        return str(self.idReplyToOffer)+' '+str(self.accept)
