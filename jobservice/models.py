2feb5ee6efbfd1e8fb279c90b17df14ca095d91b
joannamaria27 <joan.mk7@gmail.com>
2019-05-05 22:47:49 +0200

    update models (users) and update cv.html

--- jobservice/models.py
+++ jobservice/models.py
@@ -1,20 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-#class User(AbstractUser):
- #   is_company = models.BooleanField(default=False)
-  #  is_person = models.BooleanField(default=False)
-
 class Company(models.Model):
-   # user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     companyName = models.CharField(max_length= 50,default="")
     companyMail = models.EmailField(default="")
     companyPassword = models.CharField(max_length= 50,default="") 
-
+    
     def __str__(self):
         return self.companyName
 
@@ -32,6 +26,7 @@ class JobOffer(models.Model):
         return self.title+' '+str(self.companyName)
 
 class Person(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, default="")
     userName = models.CharField(max_length=10,default="")
     personMail = models.EmailField(default="")
     personPassword = models.CharField(max_length=10,default="")
@@ -40,8 +35,7 @@ class Person(models.Model):
         return self.userName
 
 class Cv(models.Model):
-    nameCv = models.CharField(max_length=50,default="")
-    userName = models.ForeignKey(Person, on_delete = models.CASCADE)
+    userName = models.OneToOneField(Person, on_delete = models.CASCADE)
     lastName = models.CharField(max_length=50,default="")
     firstName = models.CharField(max_length=50,default="")
     dateOfBirth = models.CharField(max_length=50,default="")
@@ -57,15 +51,13 @@ class ReplyToOffer(models.Model):
     idPerson = models.ForeignKey(Person, on_delete = models.CASCADE,default="")
     idOffer = models.ForeignKey(JobOffer, on_delete = models.CASCADE,default="")
     dateAdd = models.DateTimeField(auto_now_add=True)
-    cv = models.ForeignKey(Cv, on_delete = models.CASCADE, default="")
-    messForCompany = models.CharField(max_length=150,default="")
+
 
     def __str__(self):
         return str(self.idOffer)+' '+str(self.idPerson)
 
 class FeedbackAnswer(models.Model):
-    idReplyToOffer = models.ForeignKey(ReplyToOffer, on_delete = models.CASCADE,default="")
-    accept = models.BooleanField(max_length=50,default="")
+    accept = models.CharField(max_length=50,default="")
     response = models.CharField(max_length=50,default="")
 
     def __str__(self):
