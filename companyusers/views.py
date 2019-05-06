2d0b436e422e677f989bac4eeeb0805e6ab13640
joannamaria27 <joan.mk7@gmail.com>
2019-05-07 00:07:55 +0200

    update frontend

--- companyusers/views.py
+++ companyusers/views.py
@@ -16,8 +16,4 @@ def register(request):
     return render(request, 'companyusers/register.html', {'form': form})
 
 
-def logout(requset):
-    # nie wiem czy tak
-    return render(request,'companyusers/logout.html',{})
-
 
